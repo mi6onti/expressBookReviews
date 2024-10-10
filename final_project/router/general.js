5e17cca529c646f49fd3e9d9de6b8e7f969fdf97
“Mihail <mishonti@abv.bg>
2024-10-10 18:15:06 -0400

    Implement some general routes.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,20 +18,44 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    for (const isbn in books) {
+        if (isbn === req.params.isbn) {
+            return res.status(200).json({message: books[isbn].title});
+        }
+    }
+    return res.status(404).json({message: 'Book with isbn ' + req.params.isbn + 'not found'});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const found_books = [];
+    for (const isbn in books) {
+        const book = books[isbn];
+        if (book.author === req.params.author) {
+            found_books.push(book.title);
+        }
+    }
+    if (found_books.length > 0) {
+        return res.status(200).json({message: found_books.join('; ')});
+    }
+    return res.status(404).json({message: 'Books with author ' + req.params.author + ' not found'});
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const found_books = [];
+    for (const isbn in books) {
+        const book = books[isbn];
+        if (book.title === req.params.title) {
+            found_books.push(book);
+        }
+    }
+    if (found_books.length > 0) {
+        return res.status(200).json({message: found_books});
+    }
+    return res.status(404).json({message: 'Books with title ' + req.params.title + ' not found'});
+
 });
 
 //  Get book review
