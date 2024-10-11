9988cd478aaf6a0b05dd5e8476835ecec2ae8fe5
“Mihail <mishonti@abv.bg>
2024-10-11 10:23:02 -0400

    Implement reviews and register routes.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,36 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!doesExist(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -60,8 +86,17 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    for (const isbn in books) {
+        if (isbn === req.params.isbn && Object.keys(books[isbn].reviews).length > 0) {
+            return res.status(200).json({message: books[isbn].reviews});
+        }
+        else if (isbn === req.params.isbn && Object.keys(books[isbn].reviews).length === 0) {
+            return res.status(200).json({message: "Book " + books[isbn].title + " does not has any reviews yet."});
+        }
+    }
+    return res.status(404).json({message: 'Book with isbn ' + req.params.isbn + 'not found'});
 });
+
+
 
 module.exports.general = public_users;
